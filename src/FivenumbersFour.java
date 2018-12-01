8ccb646fc23d7223ae264cdca220120110735ab8
Tomek <tomasz.waligora@gmail.com>
2018-12-01 20:05:57 +0100

    commands changed to EN

--- src/FivenumbersFour.java
+++ src/FivenumbersFour.java
@@ -11,7 +11,7 @@ public class FivenumbersFour {
         int[] tab = new int[5];
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("podaj 5 liczb większych od zera");
+        System.out.println("put 5 numbers higher than zero");
 
         int counter = 0;
         while (IsArraynotComplete(tab)) {
@@ -20,13 +20,13 @@ public class FivenumbersFour {
                 tab[counter] = input;
                 counter++;
             } else {
-                System.out.println("podales ujmeną albo zero, nic nie robie");
+                System.out.println("this number is not higher than zero!");
             }
         }
         System.out.println(Arrays.toString(tab));
         Arrays.sort(tab);
         System.out.println(Arrays.toString(tab));
-        System.out.println("najmniejsza liczba to: " + tab[0] + " a największa to: " + tab[tab.length-1]);
+        System.out.println("the smallest number is: " + tab[0] + " and the highest is: " + tab[tab.length-1]);
 
     }
 
